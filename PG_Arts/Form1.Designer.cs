0c11cb20ff9cc6a690773f6a52f5766383a87710
ZILI <hassan.zili@gmail.com>
2022-05-16 16:07:01 +0100

    Ajoutez des fichiers projet.

---
+++ PG_Arts/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace PG_Arts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAfficher = new System.Windows.Forms.Button();
+            this.grd = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grd)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAfficher
+            // 
+            this.btnAfficher.Location = new System.Drawing.Point(845, 37);
+            this.btnAfficher.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAfficher.Name = "btnAfficher";
+            this.btnAfficher.Size = new System.Drawing.Size(190, 60);
+            this.btnAfficher.TabIndex = 0;
+            this.btnAfficher.Text = "Afficher";
+            this.btnAfficher.UseVisualStyleBackColor = true;
+            this.btnAfficher.Click += new System.EventHandler(this.btnAfficher_Click);
+            // 
+            // grd
+            // 
+            this.grd.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grd.Location = new System.Drawing.Point(42, 105);
+            this.grd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grd.Name = "grd";
+            this.grd.RowHeadersWidth = 82;
+            this.grd.RowTemplate.Height = 33;
+            this.grd.Size = new System.Drawing.Size(993, 362);
+            this.grd.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1082, 551);
+            this.Controls.Add(this.grd);
+            this.Controls.Add(this.btnAfficher);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grd)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAfficher;
+        private System.Windows.Forms.DataGridView grd;
+    }
+}
+
---
+++ PG_Arts/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using n = Npgsql;
+
+namespace PG_Arts
+{
+    public partial class Form1 : Form
+    {
+        static String cnxStr = "server=localhost;port=5432;user id=postgres; password=zili; database=postgres";
+        n.NpgsqlConnection cnx = new n.NpgsqlConnection(cnxStr);
+        n.NpgsqlCommand cmd;
+        n.NpgsqlDataAdapter ada;
+        DataTable dt;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAfficher_Click(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+            ada.Fill(dt);
+            grd.DataSource = dt;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmd = new n.NpgsqlCommand();
+            cmd.Connection = cnx;
+            cmd.CommandText = "SELECT * FROM public.article";
+
+            ada = new n.NpgsqlDataAdapter(cmd);
+           
+        }
+    }
+}
